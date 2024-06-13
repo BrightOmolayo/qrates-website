2035062184017c8b8367823edfcbc638c9fbe8ba
BRIGHT OMOLAYO <molayobright@gmail.com>
2024-06-13 15:35:54 +0100

    tab view

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-function toggleSection(element) {
-    var content = element.nextElementSibling;
-  
-    // Close all other sections before opening the clicked one
-    var allSections = document.querySelectorAll('.accordion-content');
-    allSections.forEach(function (section) {
-      if (section !== content && section.classList.contains('show')) {
-        section.classList.remove('show');
-      }
-    });
-  
-    // Toggle the 'show' class to display or hide the content
-    content.classList.toggle('show');
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
+function toggleSection (element) {
+  const content = element.nextElementSibling
+
+  // Close all other sections before opening the clicked one
+  const allSections = document.querySelectorAll('.accordion-content')
+  allSections.forEach(function (section) {
+    if (section !== content && section.classList.contains('show')) {
+      section.classList.remove('show')
+    }
+  })
+
+  // Toggle the 'show' class to display or hide the content
+  content.classList.toggle('show')
+}
+function dropdown () {
+  // const dropIcon = document.getElementById("bars")
+  const dropContent = document.getElementById('dropDownContent')
+
+  if (dropContent.style.display === 'none') {
+    dropContent.style.display = 'block'
+  } else {
+    dropContent.style.display = 'none'
   }
+}
